e26f7a853809997114142e6a9c810040e4a8bfae
sun sai <sunsai@yiwise.com>
2021-01-17 20:17:35 +0800

    init project

--- mini-rpc-core/src/main/java/Main.java
+++ mini-rpc-core/src/main/java/Main.java
@@ -1,7 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 
+        logger.info("start");
     }
 }
